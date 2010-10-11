aeac96e8156da6aa4af09ce063b5ce0a8d011efe
Devin Sevilla <devin@ning.com>
2010-10-11 09:46:30 -0700

    Moved the configuration constants to variables

--- src/main/java/com/ning/api/client/sample/ManualTestToken.java
+++ src/main/java/com/ning/api/client/sample/ManualTestToken.java
@@ -12,16 +12,21 @@ import com.ning.api.client.item.Token;
  */
 public class ManualTestToken
 {
+	protected final static String TEST_NETWORK = "www";
+	
     protected final static String TEST_CONSUMER_KEY = "11111111-1111-1111-1111-111111111111";
     protected final static String TEST_CONSUMER_SECRET = "11111111-1111-1111-1111-111111111111";
+    
+    protected final static String USER_EMAIL = "user@email";
+    protected final static String USER_PASS = "password";
 
     public static void main(String[] args) throws Exception
     {
         System.out.println("About to construct client, generate security token:");
-        NingClient client = new NingClient("tatutest",
+        NingClient client = new NingClient(TEST_NETWORK,
                 new AuthEntry(TEST_CONSUMER_KEY, TEST_CONSUMER_SECRET),
                 SampleBase.DEFAULT_XAPI_HOST, 80, 443);
-        Token token = client.createToken("user@email", "password");
+        Token token = client.createToken(USER_EMAIL, USER_PASS);
 
         // for output let's convert back to JSON:
         System.out.println("Call result ("+token.getClass()+") as JSON:");
