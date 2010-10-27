0bef27c6ac41b7f3e266076d17206ddff678e1c6
Tatu <tatu@ning.com>
2010-10-27 09:40:15 -0700

    ...

--- src/main/java/com/ning/api/client/sample/ManualCreateToken.java
+++ src/main/java/com/ning/api/client/sample/ManualCreateToken.java
@@ -12,20 +12,15 @@ import com.ning.api.client.item.Token;
  */
 public class ManualCreateToken
 {
-	protected final static String TEST_NETWORK = "www";
-	
-    protected final static String TEST_CONSUMER_KEY = "11111111-1111-1111-1111-111111111111";
-    protected final static String TEST_CONSUMER_SECRET = "11111111-1111-1111-1111-111111111111";
-    
     protected final static String USER_EMAIL = "user@email";
     protected final static String USER_PASS = "password";
 
     public static void main(String[] args) throws Exception
     {
         System.out.println("About to construct client, generate security token:");
-        NingClient client = new NingClient(TEST_NETWORK,
-                new ConsumerKey(TEST_CONSUMER_KEY, TEST_CONSUMER_SECRET),
-                SampleBase.DEFAULT_XAPI_HOST, 80, 443);
+        NingClient client = new NingClient(SampleBase.DEFAULT_NETWORK,
+                new ConsumerKey(SampleBase.TEST_CONSUMER_KEY, SampleBase.TEST_CONSUMER_SECRET),
+                SampleBase.DEFAULT_XAPI_HOST, SampleBase.DEFAULT_HTTP_PORT, SampleBase.DEFAULT_HTTPS_PORT);
         Token token = client.createToken(USER_EMAIL, USER_PASS);
 
         // for output let's convert back to JSON:
